5d5df37838330924fc0923367d61a2727cecfbec
crishood <crishood13@gmail.com>
2022-08-16 11:03:35 -0500

    Add animation to the header

--- src/pages/sections/Header.js
+++ src/pages/sections/Header.js
@@ -1,10 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Header = () => {
-  const [title, setTitle] = useState("<Frontend Developer />");
+  const titles = [
+    "<Frontend Developer />",
+    "<Beatmaker />",
+    "<Creator />",
+    "<Multimedia Engineer />",
+    "<M a g o de los beats />",
+    "<Ninja />",
+    "<Fullstack Developer />",
+    "<Developer />",
+  ];
+  const [title, setTitle] = useState(titles[0]);
+  let index = 2;
+  const randomTitle = () => {
+    setInterval(function () {
+      index = Math.floor(Math.random() * 8);
+      setTitle(titles[index]);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    randomTitle();
+  }, []);
+
   return (
     <header>
-      <h1>{title}</h1>
+      <h1 data-text={title} className="hero glitch layers">
+        <span>{title}</span>
+      </h1>
     </header>
   );
 };
